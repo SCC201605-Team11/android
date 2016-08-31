9af24e2dd6a20294ef0c970442df1470aaf605ba
Mak <jimmymakjieming@gmail.com>
2016-08-31 10:51:13 +0800

    Add results

---
+++ build/generated/source/buildConfig/debug/com/owncloud/android/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.owncloud.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.nextcloud.client";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10020099;
+  public static final String VERSION_NAME = "1.2.0";
+}
---
+++ oc_jb_workaround/build/generated/source/buildConfig/release/com/owncloud/android/workaround/accounts/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.owncloud.android.workaround.accounts;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.owncloud.android.workaround.accounts";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 100029;
+  public static final String VERSION_NAME = "1.0.29";
+}
